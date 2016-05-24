fa36a1492dbebb2167faaa4c63da152ed3a5ed80
Ted Shiu <tedshd@gmail.com>
2016-05-24 12:19:19 +0800

    [dev] Add file.

---
+++ img_object_fit.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @authors Ted Shiu (ted@gmail.com)
+ * @date    2016-05-24 10:27:17
+ */
+
+(function () {
+    function imgObjectFit(option) {
+        var color= option.color,
+            dom = option.dom,
+            d4img = option.d4img,
+            imgUrl = option.img || d4img,
+            type = option.type || 'contain';
+        if (!dom) {
+            console.error('not set dom');
+            return;
+        }
+        dom.style.backgroundColor = color;
+        dom.style.backgroundSize = type;
+        if (imgUrl) {
+            dom.style.backgroundImage = 'url("' + imgUrl + '")';
+        }
+        dom.style.backgroundRepeat = 'no-repeat';
+        dom.style.backgroundPosition = 'center';
+    }
+    window.imgObjectFit = imgObjectFit;
+})();
